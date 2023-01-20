017b7db5f5015394cfbc886531168e799aae2cdb
Ken Asanion <kenasanion@gmail.com>
2023-01-20 08:04:39 +0800

    Add recaptcha in settings

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   baseUrl: 'https://guest-pepmedia-dashboard.herokuapp.com',
-  apiUrl: 'https://dashboard.guestapp.ph'
+  apiUrl: 'https://dashboard.guestapp.ph',
+  recaptcha: {
+    siteKey: '6Lfq7AokAAAAAKs5kpuoP9ZVf_frb0z0Mw8klRNz',
+  },
 };
